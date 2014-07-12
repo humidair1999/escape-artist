4870507c5f2d09a99f9bed7c435b77af23915720
J. Ky Marsh <jkymarsh@gmail.com>
2014-07-12 18:01:46 -0500

    Update unit tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,46 @@
-var assert = require("assert");
+var assert = require('assert'),
+    escapeArtist = require('../index');
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+describe('.escape()', function() {
+  it('converts & into &amp;', function() {
+    assert.equal(escapeArtist.escape('&'), '&amp;');
+  });
+
+  it('converts " into &quot;', function() {
+    assert.equal(escapeArtist.escape('"'), '&quot;');
+  });
+
+  it('converts \' into &#39;', function() {
+    assert.equal(escapeArtist.escape('\''), '&#39;');
+  });
+
+  it('converts < into &lt;', function() {
+    assert.equal(escapeArtist.escape('<'), '&lt;');
+  });
+
+  it('converts > into &gt;', function() {
+    assert.equal(escapeArtist.escape('>'), '&gt;');
+  });
+});
+
+describe('.unescape()', function() {
+  it('converts &amp; into &', function() {
+    assert.equal(escapeArtist.unescape('&amp;'), '&');
+  });
+
+  it('converts &quot; into "', function() {
+    assert.equal(escapeArtist.unescape('&quot;'), '"');
+  });
+
+  it('converts &#39; into \'', function() {
+    assert.equal(escapeArtist.unescape('&#39;'), '\'');
+  });
+
+  it('converts &lt; into <', function() {
+    assert.equal(escapeArtist.unescape('&lt;'), '<');
+  });
+
+  it('converts &gt; into >', function() {
+    assert.equal(escapeArtist.unescape('&gt;'), '>');
   });
 });
